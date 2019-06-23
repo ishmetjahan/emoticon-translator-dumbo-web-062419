a938adc65ea6a2459638b4aa2809a413df36c919
Ishmet <ishmetjahan@yahoo.com>
2019-06-23 03:17:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,14 @@
 # as per the translator_spec.rb there is one arg, "the file path"
 def load_library(path)
   emoticons = YAML.load_file(path)
-  emoHash = Hash.new
+  emoHash = Hash.new 
+  emoHash["get_emoticon"]= Hash.new 
+  emoHash["get_meaning"]= Hash.new
+  emoticons.each do |word, img|
+    emoHash["get_emoticon"][img.first]= img.last 
+    emoHash["get_meaning"][img.first]= word
+  end
+  emoHash
 end
 
 def get_japanese_emoticon
