66c1379a6bed21d4febd6f775fb0b179415be2d0
Ishmet <ishmetjahan@yahoo.com>
2019-06-23 03:12:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 # require modules here
-# as per the translator_spec.rb there is one arg 
+# as per the translator_spec.rb there is one arg, "the file path"
 def load_library(path)
-  
+  emo =
 end
 
 def get_japanese_emoticon
