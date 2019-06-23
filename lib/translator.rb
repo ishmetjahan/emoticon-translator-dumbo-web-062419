6524125ca7f8d5df9bff9fc2481760c3bbe0ef4f
Ishmet <ishmetjahan@yahoo.com>
2019-06-23 03:14:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,8 @@
 # require modules here
 # as per the translator_spec.rb there is one arg, "the file path"
 def load_library(path)
-  emo =
+  emoticons = YAML.load_file(path)
+  emoHash = Hash.new
 end
 
 def get_japanese_emoticon
